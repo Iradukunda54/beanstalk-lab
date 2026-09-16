68433938fb2b8e30ff4bf95264676bded48834e8
Iradukunda54 <iradukundakevine54@gmail.com>
2026-09-16 23:52:09 +0200

    Update AppController.java
    update message

--- src/main/java/com/amalitech/lab/beanstalkapp/AppController.java
+++ src/main/java/com/amalitech/lab/beanstalkapp/AppController.java
@@ -23,7 +23,7 @@ public class AppController {
     @GetMapping("/")
     public Map<String, Object> home() {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("message", "Deployed successfully via Elastic Beanstalk");
+        body.put("message", "updated successfully via Elastic Beanstalk");
         body.put("version", appVersion);
         body.put("timestamp", Instant.now().toString());
 
